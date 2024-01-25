6c6d5e6b05ca6b93c75ca78f026bd639fa3b5517
danflo27 <florentinodc@outlook.com>
2024-01-25 16:57:08 -0500

    rename vars

--- makeList.py
+++ makeList.py
@@ -1,6 +1,9 @@
-from validatorIndices import get_node_operator, get_number_of_operators, contract_address, API_KEY, api_url
+from findOperators import get_node_operator, get_number_of_operators, contract_address, API_KEY, api_url
 
-get_number_of_operators(api_url, contract_address, API_KEY)
+# get the number of operators
+numOperators = get_number_of_operators(api_url, contract_address, API_KEY)
 
-for i in range(39):
+# get the operator data
+for i in range(numOperators):
     get_node_operator(api_url, contract_address, API_KEY, i)
+    
